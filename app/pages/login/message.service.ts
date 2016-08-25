5db82c4cfea3c46ae83ec2872fee0e07aa1cbdd4
bhawnathakur <bhawnathakur2@gmail.com>
2016-08-25 08:09:16 +0530

    final commit

---
+++ app/pages/login/message.service.ts
@@ -0,0 +1,16 @@
+import{Injectable} from '@angular/core';
+import{Http,Headers} from '@angular/http';
+import{Message} from './message.model';
+
+@Injectable()
+export class MessageService {
+constructor(private http:Http){}
+saveMessage(message:Message){
+const body = JSON.stringify(message);
+const headers = new Headers({
+   'Content-Type':'application/json'
+})
+return this.http.post("http://localhost:3000/message",body,{headers:headers});
+
+}
+}
